19f83ae81831868f0e4df9313f71795845dde25f
ESowa <it21_esow@bappassau.onmicrosoft.com>
2023-07-11 11:57:29 +0200

    Login weiter bearbeitet

--- PHP/LoginAusDatenbank.php
+++ PHP/LoginAusDatenbank.php
@@ -3,31 +3,37 @@
     //Verbindung zur Datenbank
     $conn = VerbindungHerstellen();
 
-    // Das verschlüsselte Passwort aus der Datenbank abrufen
-    $stored_password = GetUserPassword($conn, $username);
+    $mail = $_POST["mail"];
+    $passwort = $_POST["pswd"];
 
-    // Den eingegebenen Emailadresse vom Formular abrufen
-    $user_email = $_POST['email'];
-    
-    // Überprüfen ob Benutzer vorhanden
-       if(!CheckMail($user_email)){
-        echo "Benutzername nicht gefunden...Sie werden zurückgeleitet"
-        header("login.html")
-        exit;
-       }
-    // Das eingegebene Passwort vom Formular abrufen
-    $user_password = password_hash($_POST['pwd']);
-
-    // Überprüfen, ob das Passwort korrekt ist
-    if (!($user_password == $stored_password)) {
-    // Passwort ist korrekt
-    echo "Falsches Password...Sie werden zurückgeleitet"
-    header("login.html")
-    exit;
+    if(!isset($mail) && !isset($passwort)) {
+        if(!CheckEmail($user_email)) {
+            echo "<script> 
+                    alert('Benutzer nicht gefunden');
+                    window.location.href = '../login.html';
+                </script>";
+        } else {
+            $table = TabelleLesen($conn, "benutzer");
+            while($row = $table->fetch_assoc()) {
+                if($row["Email"] == $mail) {
+                    if($row["Password"] != $passwort) {
+                        echo "<script> 
+                                alert('Falsches Passwort');
+                                window.location.href = '../login.html';
+                            </script>";
+                    } else {
+                        echo "<script> 
+                                alert('Erfolgreich angemeldet');
+                                window.location.href = '../Speiseplan.html';
+                            </script>";
+                    }
+                }
+            }
+        }
+    } else {
+        echo "<script> 
+                alert('Bitte alle Felder ausfüllen');
+                window.location.href = '../login.html';
+            </script>";
     }
-
-    header("Speiseplan.html")
-    exit;
-
-
 ?>
