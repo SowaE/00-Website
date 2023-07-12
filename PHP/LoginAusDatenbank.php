203b96007c9ac2c97214e426fb6fe52cacda8dce
ESowa <it21_esow@bappassau.onmicrosoft.com>
2023-07-12 14:36:57 +0200

    Log in funktioniert jetzt

--- PHP/LoginAusDatenbank.php
+++ PHP/LoginAusDatenbank.php
@@ -3,32 +3,32 @@
     //Verbindung zur Datenbank
     $conn = VerbindungHerstellen();
 
-    $mail = $_POST["mail"];
+    $mail = $_POST["email"];
     $passwort = $_POST["pswd"];
 
-    if(!isset($mail) && !isset($passwort)) {
-        if(!CheckEmail($user_email)) {
-            echo "<script> 
-                    alert('Benutzer nicht gefunden');
-                    window.location.href = '../login.html';
-                </script>";
-        } else {
+    if(strlen($mail) != 0) {
+        if(CheckEmail($mail, $conn)) {
             $table = TabelleLesen($conn, "benutzer");
             while($row = $table->fetch_assoc()) {
                 if($row["Email"] == $mail) {
-                    if($row["Password"] != $passwort) {
-                        echo "<script> 
-                                alert('Falsches Passwort');
-                                window.location.href = '../login.html';
-                            </script>";
-                    } else {
+                    if(password_verify($passwort, $row["Password"]) || $passwort == $row["Password"]) {
                         echo "<script> 
                                 alert('Erfolgreich angemeldet');
                                 window.location.href = '../Speiseplan.html';
                             </script>";
+                    } else {
+                        echo "<script> 
+                                alert('Falsches Passwort');
+                                window.location.href = '../login.html';
+                            </script>";
                     }
                 }
             }
+        } else {
+            echo "<script> 
+                    alert('Benutzer nicht gefunden');
+                    window.location.href = '../login.html';
+                </script>";
         }
     } else {
         echo "<script> 
