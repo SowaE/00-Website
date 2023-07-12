3188cbf25e1c1ba51278e8d59c8f0b8bbb876bd5
ESowa <it21_esow@bappassau.onmicrosoft.com>
2023-07-12 14:40:10 +0200

    PkDel.php und PkNew.php gelöscht

--- PHP/AdminFunktionen/PkDel.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "../Methoden.php";
-    $conn = VerbindungHerstellen();
-    $sql = urldecode($_POST['sql']);
-    PkDel($conn, $sql);
-    mysqli_close($conn);
-?>
--- PHP/AdminFunktionen/PkNew.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "../Methoden.php";
-    $conn = VerbindungHerstellen();
-    $sql = urldecode($_POST['sql']);
-    PkNew($conn, $sql);
-    mysqli_close($conn);
-?>
